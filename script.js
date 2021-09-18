ad0a1957742eb857ba70c632c5e0989757818ed1
Gulnaz <88134441+GulnazSharshenalieva@users.noreply.github.com>
2021-09-18 11:38:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// TASK 1
+// let userName = "Gulnaz";
+// document.write(userName);
+
+
+// TASK 2
+// let userLocation = "Bishkek, Chuyskaya oblast, Kyrgyzstan";
+// document.write(userLocation);
+
+// TASK 3
+// let num1 = 200;
+// let num2 = 2021;
+// let result = num1 + num2;
+// document.write(result);
+
+// TASK 4
+
+// let userName = prompt("What is your name?");
+// document.write( "<b>" + "Your name:" + "</b>" + " " + userName + "<br>");
+
+// let userLastName = prompt("What is your last name?");
+// document.write( "<b>" + "Your last name:" + "</b>" + " " + userLastName + "<br>");
+
+// let userAge = prompt("How old are you?");
+// document.write( "<b>" + "Your age:" + "</b>" + " " + userAge + "<br>");
+
+// let userLocation = prompt("Where do you live?");
+// document.write( "<b>" + "Your city:" + "</b>" + " " + userLocation + "<br>");
+
+
+// TASK 5
+// let userName = "Gulnaz";
+// document.write(userName + "<br>");
+
+// let userLocation = "Bishkek, Kyrgyzstan";
+// document.write(userLocation);
+
+// TASK 6
+// let userName = prompt("What is your  name?");
+// document.write( "Hello," + " " + userName);
